0bc26acc176238c82174184dac7d8ceb48381b55
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-07-11 20:20:18 +0000

    Release 0.0.4

--- src/Courier.Net/Courier.cs
+++ src/Courier.Net/Courier.cs
@@ -24,7 +24,7 @@ public partial class Courier
                 { "Authorization", $"Bearer {authorizationToken}" },
                 { "X-Fern-Language", "C#" },
                 { "X-Fern-SDK-Name", "Courier.Net" },
-                { "X-Fern-SDK-Version", "0.0.3" },
+                { "X-Fern-SDK-Version", "0.0.4" },
             },
             clientOptions ?? new ClientOptions()
         );
