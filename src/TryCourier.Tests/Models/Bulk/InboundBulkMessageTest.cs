68108e9c6e1ceb0765da91b8fc87c30aa515936b
courier-codegen <codegen@users.noreply.github.com>
2026-08-13 19:56:18 +0000

    chore(api): regenerate SDK

---
+++ src/TryCourier.Tests/Models/Bulk/InboundBulkMessageTest.cs
@@ -0,0 +1,444 @@
+using System.Collections.Generic;
+using System.Text.Json;
+using TryCourier.Core;
+using TryCourier.Models;
+using TryCourier.Models.Bulk;
+
+namespace TryCourier.Tests.Models.Bulk;
+
+public class InboundBulkMessageTest : TestBase
+{
+    [Fact]
+    public void FieldRoundtrip_Works()
+    {
+        var model = new InboundBulkMessage
+        {
+            Event = "event",
+            Brand = "brand",
+            Content = new ElementalContentSugar() { Body = "body", Title = "title" },
+            Data = new Dictionary<string, JsonElement>()
+            {
+                { "foo", JsonSerializer.SerializeToElement("bar") },
+            },
+            Locale = new Dictionary<string, IReadOnlyDictionary<string, JsonElement>>()
+            {
+                {
+                    "foo",
+                    new Dictionary<string, JsonElement>()
+                    {
+                        { "foo", JsonSerializer.SerializeToElement("bar") },
+                    }
+                },
+            },
+            Override = new Dictionary<string, JsonElement>()
+            {
+                { "foo", JsonSerializer.SerializeToElement("bar") },
+            },
+            Template = "template",
+        };
+
+        string expectedEvent = "event";
+        string expectedBrand = "brand";
+        Content expectedContent = new ElementalContentSugar() { Body = "body", Title = "title" };
+        Dictionary<string, JsonElement> expectedData = new()
+        {
+            { "foo", JsonSerializer.SerializeToElement("bar") },
+        };
+        Dictionary<string, Dictionary<string, JsonElement>> expectedLocale = new()
+        {
+            {
+                "foo",
+                new Dictionary<string, JsonElement>()
+                {
+                    { "foo", JsonSerializer.SerializeToElement("bar") },
+                }
+            },
+        };
+        Dictionary<string, JsonElement> expectedOverride = new()
+        {
+            { "foo", JsonSerializer.SerializeToElement("bar") },
+        };
+        string expectedTemplate = "template";
+
+        Assert.Equal(expectedEvent, model.Event);
+        Assert.Equal(expectedBrand, model.Brand);
+        Assert.Equal(expectedContent, model.Content);
+        Assert.NotNull(model.Data);
+        Assert.Equal(expectedData.Count, model.Data.Count);
+        foreach (var item in expectedData)
+        {
+            Assert.True(model.Data.TryGetValue(item.Key, out var value));
+
+            Assert.True(JsonElement.DeepEquals(value, model.Data[item.Key]));
+        }
+        Assert.NotNull(model.Locale);
+        Assert.Equal(expectedLocale.Count, model.Locale.Count);
+        foreach (var item in expectedLocale)
+        {
+            Assert.True(model.Locale.TryGetValue(item.Key, out var value));
+
+            Assert.Equal(value.Count, model.Locale[item.Key].Count);
+            foreach (var item1 in value)
+            {
+                Assert.True(model.Locale[item.Key].TryGetValue(item1.Key, out var value1));
+
+                Assert.True(JsonElement.DeepEquals(value1, model.Locale[item.Key][item1.Key]));
+            }
+        }
+        Assert.NotNull(model.Override);
+        Assert.Equal(expectedOverride.Count, model.Override.Count);
+        foreach (var item in expectedOverride)
+        {
+            Assert.True(model.Override.TryGetValue(item.Key, out var value));
+
+            Assert.True(JsonElement.DeepEquals(value, model.Override[item.Key]));
+        }
+        Assert.Equal(expectedTemplate, model.Template);
+    }
+
+    [Fact]
+    public void SerializationRoundtrip_Works()
+    {
+        var model = new InboundBulkMessage
+        {
+            Event = "event",
+            Brand = "brand",
+            Content = new ElementalContentSugar() { Body = "body", Title = "title" },
+            Data = new Dictionary<string, JsonElement>()
+            {
+                { "foo", JsonSerializer.SerializeToElement("bar") },
+            },
+            Locale = new Dictionary<string, IReadOnlyDictionary<string, JsonElement>>()
+            {
+                {
+                    "foo",
+                    new Dictionary<string, JsonElement>()
+                    {
+                        { "foo", JsonSerializer.SerializeToElement("bar") },
+                    }
+                },
+            },
+            Override = new Dictionary<string, JsonElement>()
+            {
+                { "foo", JsonSerializer.SerializeToElement("bar") },
+            },
+            Template = "template",
+        };
+
+        string json = JsonSerializer.Serialize(model, ModelBase.SerializerOptions);
+        var deserialized = JsonSerializer.Deserialize<InboundBulkMessage>(
+            json,
+            ModelBase.SerializerOptions
+        );
+
+        Assert.Equal(model, deserialized);
+    }
+
+    [Fact]
+    public void FieldRoundtripThroughSerialization_Works()
+    {
+        var model = new InboundBulkMessage
+        {
+            Event = "event",
+            Brand = "brand",
+            Content = new ElementalContentSugar() { Body = "body", Title = "title" },
+            Data = new Dictionary<string, JsonElement>()
+            {
+                { "foo", JsonSerializer.SerializeToElement("bar") },
+            },
+            Locale = new Dictionary<string, IReadOnlyDictionary<string, JsonElement>>()
+            {
+                {
+                    "foo",
+                    new Dictionary<string, JsonElement>()
+                    {
+                        { "foo", JsonSerializer.SerializeToElement("bar") },
+                    }
+                },
+            },
+            Override = new Dictionary<string, JsonElement>()
+            {
+                { "foo", JsonSerializer.SerializeToElement("bar") },
+            },
+            Template = "template",
+        };
+
+        string element = JsonSerializer.Serialize(model, ModelBase.SerializerOptions);
+        var deserialized = JsonSerializer.Deserialize<InboundBulkMessage>(
+            element,
+            ModelBase.SerializerOptions
+        );
+        Assert.NotNull(deserialized);
+
+        string expectedEvent = "event";
+        string expectedBrand = "brand";
+        Content expectedContent = new ElementalContentSugar() { Body = "body", Title = "title" };
+        Dictionary<string, JsonElement> expectedData = new()
+        {
+            { "foo", JsonSerializer.SerializeToElement("bar") },
+        };
+        Dictionary<string, Dictionary<string, JsonElement>> expectedLocale = new()
+        {
+            {
+                "foo",
+                new Dictionary<string, JsonElement>()
+                {
+                    { "foo", JsonSerializer.SerializeToElement("bar") },
+                }
+            },
+        };
+        Dictionary<string, JsonElement> expectedOverride = new()
+        {
+            { "foo", JsonSerializer.SerializeToElement("bar") },
+        };
+        string expectedTemplate = "template";
+
+        Assert.Equal(expectedEvent, deserialized.Event);
+        Assert.Equal(expectedBrand, deserialized.Brand);
+        Assert.Equal(expectedContent, deserialized.Content);
+        Assert.NotNull(deserialized.Data);
+        Assert.Equal(expectedData.Count, deserialized.Data.Count);
+        foreach (var item in expectedData)
+        {
+            Assert.True(deserialized.Data.TryGetValue(item.Key, out var value));
+
+            Assert.True(JsonElement.DeepEquals(value, deserialized.Data[item.Key]));
+        }
+        Assert.NotNull(deserialized.Locale);
+        Assert.Equal(expectedLocale.Count, deserialized.Locale.Count);
+        foreach (var item in expectedLocale)
+        {
+            Assert.True(deserialized.Locale.TryGetValue(item.Key, out var value));
+
+            Assert.Equal(value.Count, deserialized.Locale[item.Key].Count);
+            foreach (var item1 in value)
+            {
+                Assert.True(deserialized.Locale[item.Key].TryGetValue(item1.Key, out var value1));
+
+                Assert.True(
+                    JsonElement.DeepEquals(value1, deserialized.Locale[item.Key][item1.Key])
+                );
+            }
+        }
+        Assert.NotNull(deserialized.Override);
+        Assert.Equal(expectedOverride.Count, deserialized.Override.Count);
+        foreach (var item in expectedOverride)
+        {
+            Assert.True(deserialized.Override.TryGetValue(item.Key, out var value));
+
+            Assert.True(JsonElement.DeepEquals(value, deserialized.Override[item.Key]));
+        }
+        Assert.Equal(expectedTemplate, deserialized.Template);
+    }
+
+    [Fact]
+    public void Validation_Works()
+    {
+        var model = new InboundBulkMessage
+        {
+            Event = "event",
+            Brand = "brand",
+            Content = new ElementalContentSugar() { Body = "body", Title = "title" },
+            Data = new Dictionary<string, JsonElement>()
+            {
+                { "foo", JsonSerializer.SerializeToElement("bar") },
+            },
+            Locale = new Dictionary<string, IReadOnlyDictionary<string, JsonElement>>()
+            {
+                {
+                    "foo",
+                    new Dictionary<string, JsonElement>()
+                    {
+                        { "foo", JsonSerializer.SerializeToElement("bar") },
+                    }
+                },
+            },
+            Override = new Dictionary<string, JsonElement>()
+            {
+                { "foo", JsonSerializer.SerializeToElement("bar") },
+            },
+            Template = "template",
+        };
+
+        model.Validate();
+    }
+
+    [Fact]
+    public void OptionalNullablePropertiesUnsetAreNotSet_Works()
+    {
+        var model = new InboundBulkMessage { Event = "event" };
+
+        Assert.Null(model.Brand);
+        Assert.False(model.RawData.ContainsKey("brand"));
+        Assert.Null(model.Content);
+        Assert.False(model.RawData.ContainsKey("content"));
+        Assert.Null(model.Data);
+        Assert.False(model.RawData.ContainsKey("data"));
+        Assert.Null(model.Locale);
+        Assert.False(model.RawData.ContainsKey("locale"));
+        Assert.Null(model.Override);
+        Assert.False(model.RawData.ContainsKey("override"));
+        Assert.Null(model.Template);
+        Assert.False(model.RawData.ContainsKey("template"));
+    }
+
+    [Fact]
+    public void OptionalNullablePropertiesUnsetValidation_Works()
+    {
+        var model = new InboundBulkMessage { Event = "event" };
+
+        model.Validate();
+    }
+
+    [Fact]
+    public void OptionalNullablePropertiesSetToNullAreSetToNull_Works()
+    {
+        var model = new InboundBulkMessage
+        {
+            Event = "event",
+
+            Brand = null,
+            Content = null,
+            Data = null,
+            Locale = null,
+            Override = null,
+            Template = null,
+        };
+
+        Assert.Null(model.Brand);
+        Assert.True(model.RawData.ContainsKey("brand"));
+        Assert.Null(model.Content);
+        Assert.True(model.RawData.ContainsKey("content"));
+        Assert.Null(model.Data);
+        Assert.True(model.RawData.ContainsKey("data"));
+        Assert.Null(model.Locale);
+        Assert.True(model.RawData.ContainsKey("locale"));
+        Assert.Null(model.Override);
+        Assert.True(model.RawData.ContainsKey("override"));
+        Assert.Null(model.Template);
+        Assert.True(model.RawData.ContainsKey("template"));
+    }
+
+    [Fact]
+    public void OptionalNullablePropertiesSetToNullValidation_Works()
+    {
+        var model = new InboundBulkMessage
+        {
+            Event = "event",
+
+            Brand = null,
+            Content = null,
+            Data = null,
+            Locale = null,
+            Override = null,
+            Template = null,
+        };
+
+        model.Validate();
+    }
+
+    [Fact]
+    public void CopyConstructor_Works()
+    {
+        var model = new InboundBulkMessage
+        {
+            Event = "event",
+            Brand = "brand",
+            Content = new ElementalContentSugar() { Body = "body", Title = "title" },
+            Data = new Dictionary<string, JsonElement>()
+            {
+                { "foo", JsonSerializer.SerializeToElement("bar") },
+            },
+            Locale = new Dictionary<string, IReadOnlyDictionary<string, JsonElement>>()
+            {
+                {
+                    "foo",
+                    new Dictionary<string, JsonElement>()
+                    {
+                        { "foo", JsonSerializer.SerializeToElement("bar") },
+                    }
+                },
+            },
+            Override = new Dictionary<string, JsonElement>()
+            {
+                { "foo", JsonSerializer.SerializeToElement("bar") },
+            },
+            Template = "template",
+        };
+
+        InboundBulkMessage copied = new(model);
+
+        Assert.Equal(model, copied);
+    }
+}
+
+public class ContentTest : TestBase
+{
+    [Fact]
+    public void ElementalContentSugarValidationWorks()
+    {
+        Content value = new ElementalContentSugar() { Body = "body", Title = "title" };
+        value.Validate();
+    }
+
+    [Fact]
+    public void ElementalValidationWorks()
+    {
+        Content value = new ElementalContent()
+        {
+            Elements =
+            [
+                new ElementalTextNodeWithType()
+                {
+                    Channels = ["string"],
+                    If = "if",
+                    Loop = "loop",
+                    Ref = "ref",
+                    Type = ElementalTextNodeWithTypeIntersectionMember1Type.Text,
+                },
+            ],
+            Version = "version",
+        };
+        value.Validate();
+    }
+
+    [Fact]
+    public void ElementalContentSugarSerializationRoundtripWorks()
+    {
+        Content value = new ElementalContentSugar() { Body = "body", Title = "title" };
+        string element = JsonSerializer.Serialize(value, ModelBase.SerializerOptions);
+        var deserialized = JsonSerializer.Deserialize<Content>(
+            element,
+            ModelBase.SerializerOptions
+        );
+
+        Assert.Equal(value, deserialized);
+    }
+
+    [Fact]
+    public void ElementalSerializationRoundtripWorks()
+    {
+        Content value = new ElementalContent()
+        {
+            Elements =
+            [
+                new ElementalTextNodeWithType()
+                {
+                    Channels = ["string"],
+                    If = "if",
+                    Loop = "loop",
+                    Ref = "ref",
+                    Type = ElementalTextNodeWithTypeIntersectionMember1Type.Text,
+                },
+            ],
+            Version = "version",
+        };
+        string element = JsonSerializer.Serialize(value, ModelBase.SerializerOptions);
+        var deserialized = JsonSerializer.Deserialize<Content>(
+            element,
+            ModelBase.SerializerOptions
+        );
+
+        Assert.Equal(value, deserialized);
+    }
+}
---
+++ src/TryCourier/Models/Bulk/BulkRetrieveJobResponse.cs
@@ -0,0 +1,227 @@
+using System.Collections.Frozen;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using TryCourier.Core;
+using TryCourier.Exceptions;
+using System = System;
+
+namespace TryCourier.Models.Bulk;
+
+[JsonConverter(typeof(JsonModelConverter<BulkRetrieveJobResponse, BulkRetrieveJobResponseFromRaw>))]
+public sealed record class BulkRetrieveJobResponse : JsonModel
+{
+    public required Job Job
+    {
+        get
+        {
+            this._rawData.Freeze();
+            return this._rawData.GetNotNullClass<Job>("job");
+        }
+        init { this._rawData.Set("job", value); }
+    }
+
+    /// <inheritdoc/>
+    public override void Validate()
+    {
+        this.Job.Validate();
+    }
+
+    public BulkRetrieveJobResponse() { }
+
+#pragma warning disable CS8618
+    [SetsRequiredMembers]
+    public BulkRetrieveJobResponse(BulkRetrieveJobResponse bulkRetrieveJobResponse)
+        : base(bulkRetrieveJobResponse) { }
+#pragma warning restore CS8618
+
+    public BulkRetrieveJobResponse(IReadOnlyDictionary<string, JsonElement> rawData)
+    {
+        this._rawData = new(rawData);
+    }
+
+#pragma warning disable CS8618
+    [SetsRequiredMembers]
+    BulkRetrieveJobResponse(FrozenDictionary<string, JsonElement> rawData)
+    {
+        this._rawData = new(rawData);
+    }
+#pragma warning restore CS8618
+
+    /// <inheritdoc cref="BulkRetrieveJobResponseFromRaw.FromRawUnchecked"/>
+    public static BulkRetrieveJobResponse FromRawUnchecked(
+        IReadOnlyDictionary<string, JsonElement> rawData
+    )
+    {
+        return new(FrozenDictionary.ToFrozenDictionary(rawData));
+    }
+
+    [SetsRequiredMembers]
+    public BulkRetrieveJobResponse(Job job)
+        : this()
+    {
+        this.Job = job;
+    }
+}
+
+class BulkRetrieveJobResponseFromRaw : IFromRawJson<BulkRetrieveJobResponse>
+{
+    /// <inheritdoc/>
+    public BulkRetrieveJobResponse FromRawUnchecked(
+        IReadOnlyDictionary<string, JsonElement> rawData
+    ) => BulkRetrieveJobResponse.FromRawUnchecked(rawData);
+}
+
+[JsonConverter(typeof(JsonModelConverter<Job, JobFromRaw>))]
+public sealed record class Job : JsonModel
+{
+    /// <summary>
+    /// Bulk message definition. Supports two formats: - V1 format: Requires `event`
+    /// field (event ID or notification ID) - V2 format: Optionally use `template`
+    /// (notification ID) or `content` (Elemental content) in addition to `event`
+    /// </summary>
+    public required InboundBulkMessage Definition
+    {
+        get
+        {
+            this._rawData.Freeze();
+            return this._rawData.GetNotNullClass<InboundBulkMessage>("definition");
+        }
+        init { this._rawData.Set("definition", value); }
+    }
+
+    public required long Enqueued
+    {
+        get
+        {
+            this._rawData.Freeze();
+            return this._rawData.GetNotNullStruct<long>("enqueued");
+        }
+        init { this._rawData.Set("enqueued", value); }
+    }
+
+    public required long Failures
+    {
+        get
+        {
+            this._rawData.Freeze();
+            return this._rawData.GetNotNullStruct<long>("failures");
+        }
+        init { this._rawData.Set("failures", value); }
+    }
+
+    public required long Received
+    {
+        get
+        {
+            this._rawData.Freeze();
+            return this._rawData.GetNotNullStruct<long>("received");
+        }
+        init { this._rawData.Set("received", value); }
+    }
+
+    public required ApiEnum<string, JobStatus> Status
+    {
+        get
+        {
+            this._rawData.Freeze();
+            return this._rawData.GetNotNullClass<ApiEnum<string, JobStatus>>("status");
+        }
+        init { this._rawData.Set("status", value); }
+    }
+
+    /// <inheritdoc/>
+    public override void Validate()
+    {
+        this.Definition.Validate();
+        _ = this.Enqueued;
+        _ = this.Failures;
+        _ = this.Received;
+        this.Status.Validate();
+    }
+
+    public Job() { }
+
+#pragma warning disable CS8618
+    [SetsRequiredMembers]
+    public Job(Job job)
+        : base(job) { }
+#pragma warning restore CS8618
+
+    public Job(IReadOnlyDictionary<string, JsonElement> rawData)
+    {
+        this._rawData = new(rawData);
+    }
+
+#pragma warning disable CS8618
+    [SetsRequiredMembers]
+    Job(FrozenDictionary<string, JsonElement> rawData)
+    {
+        this._rawData = new(rawData);
+    }
+#pragma warning restore CS8618
+
+    /// <inheritdoc cref="JobFromRaw.FromRawUnchecked"/>
+    public static Job FromRawUnchecked(IReadOnlyDictionary<string, JsonElement> rawData)
+    {
+        return new(FrozenDictionary.ToFrozenDictionary(rawData));
+    }
+}
+
+class JobFromRaw : IFromRawJson<Job>
+{
+    /// <inheritdoc/>
+    public Job FromRawUnchecked(IReadOnlyDictionary<string, JsonElement> rawData) =>
+        Job.FromRawUnchecked(rawData);
+}
+
+[JsonConverter(typeof(JobStatusConverter))]
+public enum JobStatus
+{
+    Created,
+    Processing,
+    Completed,
+    Error,
+}
+
+sealed class JobStatusConverter : JsonConverter<JobStatus>
+{
+    public override JobStatus Read(
+        ref Utf8JsonReader reader,
+        System::Type typeToConvert,
+        JsonSerializerOptions options
+    )
+    {
+        return JsonSerializer.Deserialize<string>(ref reader, options) switch
+        {
+            "CREATED" => JobStatus.Created,
+            "PROCESSING" => JobStatus.Processing,
+            "COMPLETED" => JobStatus.Completed,
+            "ERROR" => JobStatus.Error,
+            _ => (JobStatus)(-1),
+        };
+    }
+
+    public override void Write(
+        Utf8JsonWriter writer,
+        JobStatus value,
+        JsonSerializerOptions options
+    )
+    {
+        JsonSerializer.Serialize(
+            writer,
+            value switch
+            {
+                JobStatus.Created => "CREATED",
+                JobStatus.Processing => "PROCESSING",
+                JobStatus.Completed => "COMPLETED",
+                JobStatus.Error => "ERROR",
+                _ => throw new CourierInvalidDataException(
+                    string.Format("Invalid value '{0}' in {1}", value, nameof(value))
+                ),
+            },
+            options
+        );
+    }
+}
